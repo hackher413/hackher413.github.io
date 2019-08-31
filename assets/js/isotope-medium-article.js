37b471fe3123242e55bc5a6745b3f6a01605c35a
Sarah Manlove <smanlove29@gmail.com>
2019-08-31 13:23:55 -0700

    added prelim medium section

---
+++ assets/js/isotope-medium-article.js
@@ -0,0 +1,18 @@
+// init Isotope
+var $grid = $('.grid').isotope({
+  itemSelector: '.element-item',
+  layoutMode: 'masonry'
+});
+// bind filter button click
+$('.filters-button-group').on( 'click', 'button', function() {
+  var filterValue = $( this ).attr('data-filter');
+  $grid.isotope({ filter: filterValue });
+});
+// change is-checked class on buttons
+$('.button-group').each( function( i, buttonGroup ) {
+  var $buttonGroup = $( buttonGroup );
+  $buttonGroup.on( 'click', 'button', function() {
+    $buttonGroup.find('.is-checked').removeClass('is-checked');
+    $( this ).addClass('is-checked');
+  });
+});
