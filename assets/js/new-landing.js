3c1c8b39be0209efde3a6998ca957552f63be1a4
cegreene <cegreene@umass.edu>
2021-09-03 11:18:03 -0400

    color changes

--- assets/js/new-landing.js
+++ assets/js/new-landing.js
@@ -1,7 +1,7 @@
-var hexagon_radius = 65;
+var hexagon_radius = 45;
 var hexagon_max_absolute_speed = 0.05;
-var hexagon_space_between = 2;
-var hexagon_color = '#ccfebf';
+var hexagon_space_between = 1;
+var hexagon_color = '#b4f29c';
 var hexagon_line_width = 2;
 
 var changing_colors = false;
